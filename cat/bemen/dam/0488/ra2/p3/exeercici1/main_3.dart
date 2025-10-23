0e65dbafa9373b86cc9e47d8ef197f503a9e7e15
Diego <disaan24@bemen3.cat>
2025-10-23 20:18:07 +0200

    ejercicios 1 2 3 5 completados 4 a medias

---
+++ cat/bemen/dam/0488/ra2/p3/exeercici1/main_3.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    home: Scaffold(
+      body: Align(
+        alignment: Alignment.topLeft, // coloca todo arriba y centrado horizontalmente
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text(
+                'Esto es un recurso String',
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 10), // espacio entre texto e imagen
+              Image.asset('assets/senora.jpg', width: 250, height: 250),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
+}
